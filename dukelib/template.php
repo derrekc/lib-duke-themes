7a143167f876f84c30b231d9d74f478c014d67c3
derrekc <dlc32@duke.edu>
2012-08-13 14:04:22 -0400

    no message

--- dukelib/template.php
+++ dukelib/template.php
@@ -11,8 +11,8 @@
  * for more information on this topic.
  */
  
- function dukelib_page_alter(&$page) {
- 	$page['footer']['footer']['footer_first']['new_stuff'] = array(
+function dukelib_page_alter(&$page) {
+	$page['footer']['footer']['footer_first']['new_stuff'] = array(
  		'#type' => 'container',
  		'#attributes' => array('class' => 'my-container'),
  	);
@@ -22,4 +22,42 @@
  		'#attributes' => array('id' => 'my-heading'),
  	);
  	//dpm($page['footer']);
- }
+}
+
+function dukelib_aggregator_block_item($vars) {
+	$hrefUrl = $vars['item']->link;
+	$imgSrc = '';
+	$linkTitle = $vars['item']->title;
+
+	// get the image src
+	$match = array();
+	if (preg_match('/src="([^"]*)"/i', $vars['item']->description, $match) == 1) {
+		$imgSrc = $match[1];
+	}
+
+    $div_img_shadow = sprintf('<div class="img-shadow">'
+    	. '<a href="%s">'
+    	. '<img alt="news image" src="%s" />'
+    	. '</a>'
+    	. '</div>', $hrefUrl, $imgSrc);
+    	
+    $div_news_caption = sprintf('<div class="newsCaption">'
+    	. '<a href="%s">%s</a>'
+    	. '</div>', $hrefUrl, $linkTitle);
+    	
+	return $div_img_shadow . $div_news_caption;
+}
+
+/**
+ * implements theme_item_list().
+ */
+function dukelib_item_list($vars) {
+	return theme_item_list($vars);
+}
+
+/**
+ * Implements theme_more_link()
+ */
+function dukelib_more_link($vars) {
+	return '';
+}
